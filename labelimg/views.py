cdf0ed9eaab840d98d747a197f7561a83b8d1f01
dbckd999 <dbckd999@naver.com>
2023-07-28 12:20:48 +0900

    미사용코드 삭제

--- labelimg/views.py
+++ labelimg/views.py
@@ -3,15 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import MyModel
-from .forms import UploadFileForm, TestForm
+from .forms import UploadFileForm
 from django.views.decorators.csrf import csrf_exempt
 
 
 @csrf_exempt
 def index(request):
     if request.method == 'POST':
-        mymodel = MyModel()
         form = UploadFileForm(request.POST, request.FILES, instance=mymodel)
         if form.is_valid():
             form.save()
@@ -20,12 +18,3 @@ def index(request):
         form = UploadFileForm()
 
     return render(request, 'index.html', {'form': form})
-
-    # if request.method == 'POST':
-    #     form = TestForm(request.POST)
-    #     if form.is_valid():
-    #         form.save()
-    #         print('saved')
-
-    # form = TestForm()
-    # return render(request, 'index.html', {'form': form})
